448189a3436029bed1be24aa3c565d60980e3ae6
evilly <61359076+evillyf@users.noreply.github.com>
2022-03-02 19:32:53 -0300

    First Deploy

---
+++ main.py
@@ -0,0 +1,31 @@
+from kivy.app import App
+from kivy.lang import Builder
+from telas import *
+from botoes import *
+
+
+# parei em Ajeitando o Layout da HomePage
+
+
+
+# file onde contém a parte visual da página
+GUI = Builder.load_file("main.kv")
+
+# parte lógica do programa - padrão para iniciar o app
+class MainApp(App):
+
+    def build(self):
+        return GUI
+
+
+    def mudar_tela(self, id_tela):
+        gerenciador_telas = self.root.ids["screen_manager"]
+        gerenciador_telas.current = id_tela
+
+
+
+
+MainApp().run()
+
+
+
---
+++ telas.py
@@ -0,0 +1,12 @@
+from kivy.uix.screenmanager import Screen
+
+
+# classes de todas as telas
+
+# tela homepage
+class HomePage(Screen):
+    pass
+
+# tela ajustes
+class AjustesPage(Screen):
+    pass
