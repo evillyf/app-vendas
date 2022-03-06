935f557080f8bfb3c8ea13cd4e0f9f6fce5a86ad
evilly <61359076+evillyf@users.noreply.github.com>
2022-03-05 22:46:15 -0300

    Criando Banner de Vendas

---
+++ bannervenda.py
@@ -0,0 +1,60 @@
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.graphics import Color, Rectangle
+
+
+
+
+class BannerVenda(GridLayout):
+
+
+    def __init__(self, **kwargs):
+        self.rows = 1
+        super().__init__()
+
+        with self.canvas:
+            Color(rgb=(0, 0, 0, 1))
+            self.rec = Rectangle(size=self.size, pos=self.pos)
+        self.bind(pos=self.atualizar_rec, size=self.atualizar_rec)
+
+        cliente = kwargs['cliente']
+        foto_cliente = kwargs['foto_cliente']
+        produto = kwargs['produto']
+        foto_produto = kwargs['foto_produto']
+        data = kwargs['data']
+        unidade = kwargs['unidade']
+        quantidade = kwargs['quantidade']
+        preco = kwargs['preco']
+
+        esquerda = FloatLayout()
+        esquerda_imagem = Image(pos_hint={"right":1, "top":0.95}, size_hint=(1, 0.75), source=f"icones/fotos_clientes/{foto_cliente}")
+        esquerda_label = Label(text=cliente, size_hint=(1, 0.2), pos_hint={"right": 1, "top": 0.2})
+        esquerda.add_widget(esquerda_imagem)
+        esquerda.add_widget(esquerda_label)
+
+        meio = FloatLayout()
+        meio_imagem = Image(pos_hint={"right":1, "top":0.95}, size_hint=(1, 0.75), source=f"icones/fotos_produtos/{foto_produto}")
+        meio_label = Label(text=produto, size_hint=(1, 0.2), pos_hint={"right": 1, "top": 0.2})
+        meio.add_widget(meio_imagem)
+        meio.add_widget(meio_label)
+
+        direita = FloatLayout()
+        direita_label_data = Label(text=f"Data: {data}", size_hint=(1, 0.33), pos_hint={"right": 1, "top": 0.9})
+        direita_label_preco = Label(text=f"Preço: R${preco:,.2f}", size_hint=(1, 0.33), pos_hint={"right": 1, "top": 0.65})
+        direita_label_quantidade = Label(text=f"{quantidade} {unidade}", size_hint=(1, 0.33), pos_hint={"right": 1, "top":0.4})
+        direita.add_widget(direita_label_data)
+        direita.add_widget(direita_label_preco)
+        direita.add_widget(direita_label_quantidade)
+
+
+
+        self.add_widget(esquerda)
+        self.add_widget(meio)
+        self.add_widget(direita)
+
+
+    def atualizar_rec(self, *args):
+        self.rec.pos = self.pos
+        self.rec.size = self.size
--- main.py
+++ main.py
@@ -2,20 +2,47 @@ from kivy.app import App
 from kivy.lang import Builder
 from telas import *
 from botoes import *
+import requests
+from bannervenda import BannerVenda
 
 
 
-
-# file onde contém a parte visual da página
+# file onde contém a parte visual da página - *comentar bloco atalho: ctrl + /
 GUI = Builder.load_file("main.kv")
 
 # parte lógica do programa - padrão para iniciar o app
 class MainApp(App):
 
+    id_usuario = 1
+
     def build(self):
         return GUI
 
 
+    def on_start(self):
+        # pegar informações do usuario
+        requisicao = requests.get(f"https://aplicativovendas-6d2e9-default-rtdb.firebaseio.com/{self.id_usuario}.json")
+        requisicao.dic = requisicao.json()
+
+        # preencher foto de perfil
+        avatar = requisicao.dic['avatar']
+        foto_perfil = self.root.ids["foto_perfil"]
+        foto_perfil.source = f"icones/fotos_perfil/{avatar}"
+
+        # preencher lista de vendas
+        try:
+            vendas = requisicao.dic['vendas'][1:]
+            for venda in vendas:
+               banner = BannerVenda(cliente=venda['cliente'], foto_cliente=venda['foto_cliente'],
+                                    produto=venda['produto'], foto_produto=venda['foto_produto'],
+                                    data=venda['data'], preco=venda['preco'], unidade=venda['unidade'],
+                                    quantidade=venda['quantidade'])
+               pagina_homepage = self.root.ids["homepage"]
+               lista_vendas = pagina_homepage.ids["lista_vendas"]
+               lista_vendas.add_widget(banner)
+
+        except:
+            pass
     def mudar_tela(self, id_tela):
         gerenciador_telas = self.root.ids["screen_manager"]
         gerenciador_telas.current = id_tela
