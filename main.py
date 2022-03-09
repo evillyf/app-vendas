3ac8591a57422b70fba4a859a3104400cb82f36b
Evilly <61359076+evillyf@users.noreply.github.com>
2022-03-09 13:15:16 -0300

    Foto Perfil

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ from telas import *
 from botoes import *
 import requests
 from bannervenda import BannerVenda
+import os
 
-
-
+# parei em Atualizar Informações no Banco de Dados - Mudar foto de Perfil
 # file onde contém a parte visual da página - *comentar bloco atalho: ctrl + /
 GUI = Builder.load_file("main.kv")
 
 # parte lógica do programa - padrão para iniciar o app
+
+
 class MainApp(App):
 
     id_usuario = 1
@@ -18,10 +20,25 @@ class MainApp(App):
     def build(self):
         return GUI
 
-
     def on_start(self):
+        # carregar as fotos de perfil
+        arquivos = os.listdir("icones/fotos_perfil")
+        pagina_fotoperfil = self.root.ids["fotoperfilpage"]
+        lista_fotos = pagina_fotoperfil.ids["lista_fotos_perfil"]
+
+        for foto in arquivos:
+            imagem = ImageButton(source=f"icones/fotos_perfil/{foto}", on_release=self.mudar_foto_perfil)
+            lista_fotos.add_widget(imagem)
+
+        # carrega as infos do usuario
+        self.carregar_infos_usuario()
+
+
+
+    def carregar_infos_usuario(self):
         # pegar informações do usuario
-        requisicao = requests.get(f"https://aplicativovendas-6d2e9-default-rtdb.firebaseio.com/{self.id_usuario}.json")
+        requisicao = requests.get(
+            f"https://aplicativovendas-6d2e9-default-rtdb.firebaseio.com/{self.id_usuario}.json")
         requisicao.dic = requisicao.json()
 
         # preencher foto de perfil
@@ -32,20 +49,29 @@ class MainApp(App):
         # preencher lista de vendas
         try:
             vendas = requisicao.dic['vendas'][1:]
+            pagina_homepage = self.root.ids["homepage"]
+            lista_vendas = pagina_homepage.ids["lista_vendas"]
             for venda in vendas:
                banner = BannerVenda(cliente=venda['cliente'], foto_cliente=venda['foto_cliente'],
                                     produto=venda['produto'], foto_produto=venda['foto_produto'],
                                     data=venda['data'], preco=venda['preco'], unidade=venda['unidade'],
                                     quantidade=venda['quantidade'])
-               pagina_homepage = self.root.ids["homepage"]
-               lista_vendas = pagina_homepage.ids["lista_vendas"]
                lista_vendas.add_widget(banner)
 
         except:
-            pass
+            pass       
+
+
+
     def mudar_tela(self, id_tela):
         gerenciador_telas = self.root.ids["screen_manager"]
         gerenciador_telas.current = id_tela
+
+
+
+    def mudar_foto_perfil(self, *args):
+        print("Mudar foto perfil")
+
 
 
 
