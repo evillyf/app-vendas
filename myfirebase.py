723e672fbaabf0805f88634948d4b77c07d52a8f
Evilly <61359076+evillyf@users.noreply.github.com>
2022-03-18 14:13:01 -0300

    Validação de email e senha

--- myfirebase.py
+++ myfirebase.py
@@ -1,4 +1,5 @@
 import requests 
+from kivy.app import App
 
 
 
@@ -9,10 +10,19 @@ class MyFirebase():
         link = f"https://identitytoolkit.googleapis.com/v1/accounts:signUp?key={self.API_KEY}"
 
         info = {"email": email,
-                "senha": senha,
+                "password": senha,
                 "returnSecureToken": True}
         requisicao = requests.post(link, data=info)
         requisicao_dic = requisicao.json()
+        
+        if requisicao.ok:
+            print("Usuário Criado")
+        else:
+            mensagem_erro = requisicao_dic["error"]["message"]
+            meu_aplicativo = App.get_running_app()
+            pagina_login = meu_aplicativo.root.ids["loginpage"]
+            pagina_login.ids["mensagem_login"].text = mensagem_erro
+            pagina_login.ids["mensagem_login"].color = (1, 0, 0 , 1)
         print(requisicao_dic)
 
     def fazer_login(self, email, senha):
