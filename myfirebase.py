279ca06c225374fc406c746db74075d7f9b41591
evilly <61359076+evillyf@users.noreply.github.com>
2022-03-20 16:24:18 -0300

    Autenticação Login

--- myfirebase.py
+++ myfirebase.py
@@ -45,8 +45,38 @@ class MyFirebase():
         print(requisicao_dic)
 
     def fazer_login(self, email, senha):
-        pass
+        link = f"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={self.API_KEY}"
+        info = {"email": email,
+                "password": senha,
+                "returnSecureToken": True}
+        requisicao = requests.post(link, data=info)
+        requisicao_dic = requisicao.json()
 
+        if requisicao.ok:
+            refresh_token = requisicao_dic["refreshToken"]
+            local_id = requisicao_dic["localId"]
+            id_token = requisicao_dic["idToken"]
+
+            meu_aplicativo = App.get_running_app()
+            meu_aplicativo.local_id = local_id
+            meu_aplicativo.id_token = id_token
+
+            with open("refreshtoken.txt", "w") as arquivo:
+                arquivo.write(refresh_token)
+
+
+            link = f"https://aplicativovendas-6d2e9-default-rtdb.firebaseio.com/{local_id}.json"
+
+
+            meu_aplicativo.carregar_infos_usuario()
+            meu_aplicativo.mudar_tela("homepage")
+
+        else:
+            mensagem_erro = requisicao_dic["error"]["message"]
+            meu_aplicativo = App.get_running_app()
+            pagina_login = meu_aplicativo.root.ids["loginpage"]
+            pagina_login.ids["mensagem_login"].text = mensagem_erro
+            pagina_login.ids["mensagem_login"].color = (1, 0, 0 , 1)
 
     def trocar_token(self, refresh_token):
         link = f"https://securetoken.googleapis.com/v1/token?key={self.API_KEY}"
