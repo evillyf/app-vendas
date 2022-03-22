0580c14c928d65405bbf882c88b226f133fc6cc8
Evilly <61359076+evillyf@users.noreply.github.com>
2022-03-22 11:30:51 -0300

    adicionar vendas

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from urllib import request
 from kivy.app import App
 from kivy.lang import Builder
 from telas import *
@@ -10,16 +9,11 @@ from functools import partial
 from myfirebase import MyFirebase
 from bannervendedor import BannerVendedor
 
-# banco de dados: https://console.firebase.google.com/
-# file onde contém a parte visual da página - *comentar bloco atalho: ctrl + /
+
+# fotos avatar em flat icon 24 px -organizar fileiras F e M
+
 GUI = Builder.load_file("main.kv")
-
-# parte lógica do programa - padrão para iniciar o app
-
-
 class MainApp(App):
-
-    id_usuario = 1
 
     def build(self):
         self.firebase = MyFirebase()
@@ -30,75 +24,64 @@ class MainApp(App):
         arquivos = os.listdir("icones/fotos_perfil")
         pagina_fotoperfil = self.root.ids["fotoperfilpage"]
         lista_fotos = pagina_fotoperfil.ids["lista_fotos_perfil"]
-
         for foto in arquivos:
             imagem = ImageButton(source=f"icones/fotos_perfil/{foto}", on_release=partial(self.mudar_foto_perfil, foto))
             lista_fotos.add_widget(imagem)
-
         # carrega as infos do usuario
         self.carregar_infos_usuario()
-
-
 
     def carregar_infos_usuario(self):
         try:
             with open("refreshtoken.txt", "r") as arquivo:
                 refresh_token = arquivo.read()
-
             local_id, id_token = self.firebase.trocar_token(refresh_token)
             self.local_id = local_id
             self.id_token = id_token
-            #pegar informações do usuario
-            requisicao = requests.get(
-                f"https://aplicativovendas-6d2e9-default-rtdb.firebaseio.com/{self.local_id}.json")
-            requisicao.dic = requisicao.json()
+
+            # pegar informações do usuario
+            requisicao = requests.get(f"https://aplicativovendas-6d2e9-default-rtdb.firebaseio.com/{self.local_id}.json")
+            requisicao_dic = requisicao.json()
 
             # preencher foto de perfil
-            avatar = requisicao.dic['avatar']
+            avatar = requisicao_dic['avatar']
             self.avatar = avatar
             foto_perfil = self.root.ids["foto_perfil"]
             foto_perfil.source = f"icones/fotos_perfil/{avatar}"
 
-            #preencher id unico
-            id_vendedor = requisicao.dic['id_vendedor']
+            # preencher o ID unico
+            id_vendedor = requisicao_dic['id_vendedor']
             self.id_vendedor = id_vendedor
             pagina_ajustes = self.root.ids["ajustespage"]
             pagina_ajustes.ids["id_vendedor"].text = f"Seu ID Único: {id_vendedor}"
 
-
-            #preencher total de vendas
-            total_vendas = requisicao.dic['total_vendas']
+            # preencher o total de vendas
+            total_vendas = requisicao_dic['total_vendas']
             self.total_vendas = total_vendas
             homepage = self.root.ids["homepage"]
             homepage.ids["label_total_vendas"].text = f"[color=#000000]Total de Vendas:[/color] [b]R${total_vendas}[/b]"
 
-            #preencher equipe
-            self.equipe = requisicao.dic['equipe']
-
-
+            # preencher equipe
+            self.equipe = requisicao_dic["equipe"]
 
             # preencher lista de vendas
             try:
-                vendas = requisicao.dic['vendas'][1:]
-                self.vendas = vendas 
+                vendas = requisicao_dic['vendas'][1:]
+                self.vendas = vendas
                 pagina_homepage = self.root.ids["homepage"]
                 lista_vendas = pagina_homepage.ids["lista_vendas"]
                 for venda in vendas:
-                   banner = BannerVenda(cliente=venda['cliente'], foto_cliente=venda['foto_cliente'],
-                                        produto=venda['produto'], foto_produto=venda['foto_produto'],
-                                        data=venda['data'], preco=venda['preco'], unidade=venda['unidade'],
-                                        quantidade=venda['quantidade'])
+                    banner = BannerVenda(cliente=venda["cliente"], foto_cliente=venda["foto_cliente"],
+                                         produto=venda["produto"], foto_produto=venda["foto_produto"],
+                                         data=venda['data'], preco=venda['preco'],
+                                         unidade=venda['unidade'], quantidade=venda["quantidade"])
 
-                   lista_vendas.add_widget(banner)
-
-
+                    lista_vendas.add_widget(banner)
             except:
                 pass
 
-            #preencher equipe de vendedores
-            equipe = requisicao.dic['equipe']
+            # preencher equipe de vendedores
+            equipe = requisicao_dic["equipe"]
             lista_equipe = equipe.split(",")
-
             pagina_listavendedores = self.root.ids["listarvendedorespage"]
             lista_vendedores = pagina_listavendedores.ids["lista_vendedores"]
 
@@ -107,67 +90,51 @@ class MainApp(App):
                     banner_vendedor = BannerVendedor(id_vendedor=id_vendedor_equipe)
                     lista_vendedores.add_widget(banner_vendedor)
 
-
             self.mudar_tela("homepage")
         except:
             pass
-
-
-
-
-
 
     def mudar_tela(self, id_tela):
         gerenciador_telas = self.root.ids["screen_manager"]
         gerenciador_telas.current = id_tela
 
-
-
     def mudar_foto_perfil(self, foto, *args):
         foto_perfil = self.root.ids["foto_perfil"]
-        foto_perfil.source = f"icones/fotos_perfil/{foto}"        
-
+        foto_perfil.source = f"icones/fotos_perfil/{foto}"
 
         info = f'{{"avatar": "{foto}"}}'
-        requisicao = requests.patch(f"https://aplicativovendas-6d2e9-default-rtdb.firebaseio.com/{self.local_id}.json", data=info)
+        requisicao = requests.patch(f"https://aplicativovendas-6d2e9-default-rtdb.firebaseio.com/{self.local_id}.json",
+                                    data=info)
 
         self.mudar_tela("ajustespage")
-
-    
 
     def adicionar_vendedor(self, id_vendedor_adicionado):
         link = f'https://aplicativovendas-6d2e9-default-rtdb.firebaseio.com/.json?orderBy="id_vendedor"&equalTo="{id_vendedor_adicionado}"'
         requisicao = requests.get(link)
         requisicao_dic = requisicao.json()
-        print(requisicao_dic)
-        
+
         pagina_adicionarvendedor = self.root.ids["adicionarvendedorpage"]
         mensagem_texto = pagina_adicionarvendedor.ids["mensagem_outrovendedor"]
 
         if requisicao_dic == {}:
             mensagem_texto.text = "Usuário não encontrado"
-        else: 
+        else:
             equipe = self.equipe.split(",")
             if id_vendedor_adicionado in equipe:
                 mensagem_texto.text = "Vendedor já faz parte da equipe"
             else:
                 self.equipe = self.equipe + f",{id_vendedor_adicionado}"
                 info = f'{{"equipe": "{self.equipe}"}}'
-                requests.patch(f"https://aplicativovendas-6d2e9-default-rtdb.firebaseio.com/{self.local_id}.json", data=info)
-                mensagem_texto.text = "Vendedor adicionado com sucesso!"
-
-                # adicionar um novo bannner na lista de vendedores
+                requests.patch(f"https://aplicativovendas-6d2e9-default-rtdb.firebaseio.com/{self.local_id}.json",
+                               data=info)
+                mensagem_texto.text = "Vendedor Adicionado com Sucesso"
+                # adicionar um novo banner na lista de vendedores
                 pagina_listavendedores = self.root.ids["listarvendedorespage"]
                 lista_vendedores = pagina_listavendedores.ids["lista_vendedores"]
                 banner_vendedor = BannerVendedor(id_vendedor=id_vendedor_adicionado)
-                lista_vendedores.add_widget(banner_vendedor)            
-                
+                lista_vendedores.add_widget(banner_vendedor)
 
-
+                #mudar tela depois que add um vendedor 
+                #self.mudar_tela("ajustespage")
 
 MainApp().run()
-
-
-
-
-
