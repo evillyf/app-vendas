5a75a96b9e0bd5f9f64cedf429162403b0fa1ed8
Evilly <61359076+evillyf@users.noreply.github.com>
2022-03-28 13:42:17 -0300

    importando o certifi

--- main.py
+++ main.py
@@ -6,10 +6,13 @@ from botoes import *
 import requests
 from bannervenda import BannerVenda
 import os
+import certifi
 from functools import partial
 from myfirebase import MyFirebase
 from bannervendedor import BannerVendedor
 from datetime import date
+
+os.environ['SSL_CERT_FILE'] = certifi.where()
 
 GUI = Builder.load_file("main.kv")
 class MainApp(App):
